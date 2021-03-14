04db2698503d304227351588dcf4d270058d3dc2
David Farelas <david.farelas.dev@gmail.com>
2021-03-13 20:21:37 -0600

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+    category
+  )}&limit=10&api_key=AhuVePISG9iI6O8zI2DSKodO6Oy2bXMq`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+
+  const gifs = data.map((img) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url,
+    };
+  });
+
+  return gifs;
+};
